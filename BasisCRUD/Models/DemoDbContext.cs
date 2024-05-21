3e8947fac9af086fac551d18c4d0efd7bb253428
bankbank123 <chidchanun.bank@mail.kmutt.ac.th>
2024-05-21 13:21:16 +0700

    21/5/2567

--- BasisCRUD/Models/DemoDbContext.cs
+++ BasisCRUD/Models/DemoDbContext.cs
@@ -22,7 +22,6 @@ public partial class DemoDbContext : DbContext
     public virtual DbSet<Publisher> Publishers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server = DESKTOP-7R12NSA\\SQLEXPRESS01; Database = demo_db; Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
